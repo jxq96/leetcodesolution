c5323d693224c1359a7a5bc5ea06c1ca0c7cfcf0
jxq96 <jxq96@mail.ustc.edu.cn>
2019-03-27 21:57:16 +0800

    non recursion solution

---
+++ inorderTraversal.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> list[int]:
+        p = root
+        stack = []
+        res = []
+        while p is not None or len(stack) != 0:
+            while p is not None:
+                stack.append(p)
+                p = p.left
+            if len(stack) != 0:
+                p = stack.pop()
+                res.append(p.val)
+                p = p.right
+        return res
